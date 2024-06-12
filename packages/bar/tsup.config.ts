7f991d34885a3149bef923837c3308181e4c2d9e
ice breaker <1324318532@qq.com>
2024-06-12 09:17:14 +0800

    chore: add @icebreakers/bar and change type to module

--- packages/bar/tsup.config.ts
+++ packages/bar/tsup.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'], // , 'src/cli.ts'],
   shims: true,
+  format: ['cjs', 'esm'],
+  clean: true,
+  dts: true,
 })
