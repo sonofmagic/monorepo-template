3e71ba440eb629e0764de21a1878b3a43d9c541c
ice breaker <1324318532@qq.com>
2024-08-18 01:28:01 +0800

    chore: bump @icebreakers/monorepo version

--- packages/monorepo/src/cli.ts
+++ packages/monorepo/src/cli.ts
@@ -11,33 +11,39 @@ const __dirname = path.dirname(__filename)
 const assetsDir = path.join(__dirname, './assets')
 const cwd = process.cwd()
 
-const list = await fs.readdir(assetsDir)
-for (const item of list) {
-  if (item === 'package.json') {
-    const sourcePath = path.resolve(assetsDir, item)
-    const targetPath = path.resolve(cwd, item)
-    if (await fs.exists(targetPath) && await fs.exists(sourcePath)) {
-      const sourcePkgJson = await fs.readJson(sourcePath) as PackageJson
-      const targetPkgJson = await fs.readJson(targetPath) as PackageJson
+async function main() {
+  const list = await fs.readdir(assetsDir)
+  for (const item of list) {
+    if (item === 'package.json') {
+      const sourcePath = path.resolve(assetsDir, item)
+      const targetPath = path.resolve(cwd, item)
+      if (await fs.exists(targetPath) && await fs.exists(sourcePath)) {
+        const sourcePkgJson = await fs.readJson(sourcePath) as PackageJson
+        const targetPkgJson = await fs.readJson(targetPath) as PackageJson
 
-      const deps = get(sourcePkgJson, 'dependencies', { default: {} })
-      const devDeps = get(sourcePkgJson, 'devDependencies', { default: {} })
+        const deps = get(sourcePkgJson, 'dependencies', { default: {} })
+        const devDeps = get(sourcePkgJson, 'devDependencies', { default: {} })
 
-      Object.entries(deps).forEach((x) => {
-        set(targetPkgJson, x[0], x[1])
-      })
-      Object.entries(devDeps).forEach((x) => {
-        set(targetPkgJson, x[0], x[1])
-      })
-      await fs.writeJson(targetPath, targetPkgJson, {
-        spaces: 2,
-      })
+        Object.entries(deps).forEach((x) => {
+          set(targetPkgJson, x[0], x[1])
+        })
+        Object.entries(devDeps).forEach((x) => {
+          set(targetPkgJson, x[0], x[1])
+        })
+        await fs.writeJson(targetPath, targetPkgJson, {
+          spaces: 2,
+        })
+      }
+    }
+    else {
+      await fs.copy(
+        path.resolve(assetsDir, item),
+        path.resolve(cwd, item),
+      )
     }
   }
-  else {
-    await fs.copy(
-      path.resolve(assetsDir, item),
-      path.resolve(cwd, item),
-    )
-  }
 }
+
+main().then(() => {
+  console.log('@icebreakers/monorepo ok!')
+})
