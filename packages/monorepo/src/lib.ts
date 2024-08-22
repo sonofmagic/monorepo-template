34d7efe63f3801ab5c01a7344a20c7db017ec48d
ice breaker <1324318532@qq.com>
2024-08-22 11:14:20 +0800

    chore: commit @icebreakers/monorepo

--- packages/monorepo/src/lib.ts
+++ packages/monorepo/src/lib.ts
@@ -6,8 +6,15 @@ import type { PackageJson } from 'pkg-types'
 import get from 'get-value'
 import set from 'set-value'
 import klaw from 'klaw'
+import mm from 'micromatch'
+import PQueue from 'p-queue'
 import { GitClient } from './git'
 import type { CliOpts } from './types'
+import { getTargets } from './targets'
+
+// const controller = new AbortController()
+
+const queue = new PQueue({ concurrency: 1 })
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -17,57 +24,60 @@ const assetsDir = path.join(__dirname, '../assets')
 const cwd = process.cwd()
 
 export async function main(opts: CliOpts) {
-  const { outDir = '' } = opts
+  const { outDir = '', raw } = opts
   const absOutDir = path.isAbsolute(outDir) ? outDir : path.join(cwd, outDir)
   const gitClient = new GitClient({
     baseDir: cwd,
   })
   const repoName = await gitClient.getRepoName()
-
+  const targets = getTargets(raw)
   for await (const file of klaw(assetsDir, {
-    filter() {
-      // path.relative(assetsDir, p).startsWith()
-      return true
+    filter(p) {
+      const str = path.relative(assetsDir, p)
+      return mm.isMatch(str, targets)
     },
   })) {
-    if (file.stats.isFile()) {
-      const relPath = path.relative(assetsDir, file.path)
-      const targetPath = path.resolve(absOutDir, relPath)
-      // const basename = path.basename(file.path)
-      if (relPath === 'package.json') {
-        const sourcePath = file.path
-        if (await fs.exists(targetPath) && await fs.exists(sourcePath)) {
-          const sourcePkgJson = await fs.readJson(sourcePath) as PackageJson
-          const targetPkgJson = await fs.readJson(targetPath) as PackageJson
+    await queue.add(async () => {
+      if (file.stats.isFile()) {
+        const relPath = path.relative(assetsDir, file.path)
+        const targetPath = path.resolve(absOutDir, relPath)
+        // const basename = path.basename(file.path)
+        if (relPath === 'package.json') {
+          const sourcePath = file.path
+          if (await fs.exists(targetPath) && await fs.exists(sourcePath)) {
+            const sourcePkgJson = await fs.readJson(sourcePath) as PackageJson
+            const targetPkgJson = await fs.readJson(targetPath) as PackageJson
 
-          const deps = get(sourcePkgJson, 'dependencies', { default: {} })
-          const devDeps = get(sourcePkgJson, 'devDependencies', { default: {} })
+            const deps = get(sourcePkgJson, 'dependencies', { default: {} })
+            const devDeps = get(sourcePkgJson, 'devDependencies', { default: {} })
 
-          Object.entries(deps).forEach((x) => {
-            set(targetPkgJson, `dependencies.${x[0]}`, x[1], { preservePaths: false })
-          })
-          Object.entries(devDeps).forEach((x) => {
-            set(targetPkgJson, `devDependencies.${x[0]}`, x[1], { preservePaths: false })
-          })
-          await fs.writeJson(targetPath, targetPkgJson, {
+            Object.entries(deps).forEach((x) => {
+              set(targetPkgJson, `dependencies.${x[0]}`, x[1], { preservePaths: false })
+            })
+            Object.entries(devDeps).forEach((x) => {
+              set(targetPkgJson, `devDependencies.${x[0]}`, x[1], { preservePaths: false })
+            })
+
+            await fs.writeJson(targetPath, targetPkgJson, {
+              spaces: 2,
+            })
+          }
+        }
+        else if (relPath === '.changeset/config.json' && repoName && await fs.exists(file.path)) {
+          const changesetJson = await fs.readJson(file.path)
+          set(changesetJson, 'changelog.1.repo', repoName)
+          await fs.ensureDir(path.dirname(targetPath))
+          await fs.writeJson(targetPath, changesetJson, {
             spaces: 2,
           })
         }
+        else {
+          await fs.copy(
+            file.path,
+            path.resolve(absOutDir, relPath),
+          )
+        }
       }
-      else if (relPath === '.changeset/config.json' && repoName && await fs.exists(file.path)) {
-        const changesetJson = await fs.readJson(file.path)
-        set(changesetJson, 'changelog.1.repo', repoName)
-        await fs.ensureDir(path.dirname(targetPath))
-        await fs.writeJson(targetPath, changesetJson, {
-          spaces: 2,
-        })
-      }
-      else {
-        await fs.copy(
-          file.path,
-          path.resolve(absOutDir, relPath),
-        )
-      }
-    }
+    })
   }
 }
