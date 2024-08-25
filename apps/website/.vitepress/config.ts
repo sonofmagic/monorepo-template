59629529be03eb5f5151433e26802239286d809e
ice breaker <1324318532@qq.com>
2024-08-25 15:32:36 +0800

    chore: bump version

--- apps/website/.vitepress/config.ts
+++ apps/website/.vitepress/config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     // ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'github', link: 'https://github.com/sonofmagic/monorepo-template' },
     ],
   },
 })
