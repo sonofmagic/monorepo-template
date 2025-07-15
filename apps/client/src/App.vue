7b0dd8d15d51c025d1d99285e48d1b508b6eca82
ice breaker <1324318532@qq.com>
2025-07-15 10:39:50 +0800

    chore(deps): upgrade

---
+++ apps/client/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { HelloWorld } from '@icebreakers/vue-lib-template'
+</script>
+
+<template>
+  <div>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<style scoped></style>
