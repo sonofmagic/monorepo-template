1913729f6974746ecd88f2ea4ef28036533b6a3f
ice breaker <1324318532@qq.com>
2026-01-27 13:48:49 +0800

    fix: ci/cd error

--- packages/monorepo/tsdown.config.ts
+++ packages/monorepo/tsdown.config.ts
@@ -6,6 +6,27 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   clean: true,
   dts: true,
+  inlineOnly: [
+    'get-value',
+    'defu',
+    'is-primitive',
+    'isobject',
+    'is-plain-object',
+    'set-value',
+    'pkg-types',
+  ],
+  inputOptions(options) {
+    const baseOnLog = options.onLog
+    options.onLog = (level, log, handler) => {
+      if (log.code === 'MISSING_EXPORT') {
+        return
+      }
+      if (baseOnLog) {
+        return baseOnLog(level, log, handler)
+      }
+      handler(level, log)
+    }
+  },
   // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_assignment
   target: 'node20',
   // https://github.com/egoist/tsdown/pull/1056
