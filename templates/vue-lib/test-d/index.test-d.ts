2fb3487689954383015c9b5532ec3eec4e77646d
ice breaker <1324318532@qq.com>
2026-06-26 16:17:53 +0800

    fix(vue-lib): update generated declaration entry

--- templates/vue-lib/test-d/index.test-d.ts
+++ templates/vue-lib/test-d/index.test-d.ts
@@ -1,6 +1,6 @@
 import type { Component } from 'vue'
 import { expectAssignable } from 'tsd'
 // eslint-disable-next-line antfu/no-import-dist
-import { HelloWorld } from '../dist/src/index'
+import { HelloWorld } from '../dist/index'
 
 expectAssignable<Component>(HelloWorld)
